41b4d26f66c7a64c3307393287d30d449e598e07
deeper.myeong.lee <deeper.myeong.lee@gmail.com>
2013-08-13 02:10:17 -0400

    Added building list

--- Web/tubeSearch.php
+++ Web/tubeSearch.php
@@ -7,3 +7,4 @@ The closest UMD building is
 </div>
 <img src="images/buildingmap_tn.jpg" alt="London underground map"/>
 <p class="small"><a href="http://map.umd.edu/map/" target="_blank" >View larger map</a></p>
+<p class="small"><a href="buildings.php" target="_blank">View building list</a></p>
